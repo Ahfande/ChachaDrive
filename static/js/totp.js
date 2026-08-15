7f9bba4ef517b58519fdfd32e5fde30bf684d5bd
Ahfande <157612444+Ahfande@users.noreply.github.com>
2026-08-15 16:47:10 +0700

    Update totp.js

--- static/js/totp.js
+++ static/js/totp.js
@@ -2,7 +2,7 @@
  * TOTP.JS - Terintegrasi dengan Backend Django
  */
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = window.location.origin + '/api';
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log('🚀 Halaman TOTP siap!');
